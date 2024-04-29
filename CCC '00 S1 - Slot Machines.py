b6080d089e56b07c8a61d2082e64afae7b4e645c
Jason He <151975056+BerryMachine@users.noreply.github.com>
2024-04-29 16:45:32 -0400

    Update CCC '00 S1 - Slot Machines.py
    Fix comments

--- CCC '00 S1 - Slot Machines.py
+++ CCC '00 S1 - Slot Machines.py
@@ -1,33 +1,28 @@
-# get number of quarters as input
+# Get number of quarters as input
 quarters = int(input())
 
-# get number of times each machine has been played
+# Get number of times each machine has been played
 first = int(input())
 second = int(input())
 third = int(input())
 
-#Keeping track of plays
+# Keep track of plays
 plays = 0
 
 while True:
-  #Adding plays to each machine based on number of quarters
+  # Add plays to each machine based on number of quarters
   remainder = quarters % 3
   first += quarters // 3 
   second += quarters // 3
   third += quarters // 3
 
-  #Dealing with left over plays after distribution
+  # Deal with left over plays after distribution
   if remainder == 1: first += 1
   if remainder == 2: first += 1; second += 1
-
-  #The plays is basically the amount of quarters
   plays += quarters
-
-  #making quarters zero because the quarter are used up in the plays
   quarters = 0
 
-  
-  #checking for wins if get more squart to distribute again
+  # Calculate quarters gained through wins
   if first // 35 > 0:
     quarters += (first // 35)*30
     first -= 35*(first // 35)
@@ -42,10 +37,9 @@ while True:
     quarters += (third // 10)*9
     third -= 10*(third // 10)
 
-  #If there are no more quarter, there can be no plays
+  # Stop when the number of quarters reach 0
   if quarters == 0:
     break
 
-#after the plays are summed up, this is printing it
 print(f"Martha plays {plays} times before going broke.")
 
