24ef6465134696735f98d4fae4b5bfece583cde0
Jason He <151975056+BerryMachine@users.noreply.github.com>
2024-04-29 16:50:10 -0400

    Update CCC '11 S1 - English or French?.py
    make comments consistent

--- CCC '11 S1 - English or French?.py
+++ CCC '11 S1 - English or French?.py
@@ -2,17 +2,17 @@ num_of_s = 0
 num_of_t = 0
 
 for N in range(int(input())):
-  # Split string into array of letters
+  # split string into array of letters
   line = [*input()]
 
-  # Count number of each letter
+  # count number of each letter
   for letter in line:
     if letter == "T" or letter == "t":
       num_of_t += 1
     if letter == 'S' or letter == "s":
       num_of_s += 1
 
-# Output most likely language
+# output most likely language
 if num_of_t > num_of_s:
   print("English")
 else: print("French")
