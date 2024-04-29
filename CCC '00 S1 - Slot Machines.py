ffdf48c3712af4c7fb4860484c087160fefd6907
Jason He <151975056+BerryMachine@users.noreply.github.com>
2024-04-29 16:48:35 -0400

    Update CCC '00 S1 - Slot Machines.py
    Make comments consistent

--- CCC '00 S1 - Slot Machines.py
+++ CCC '00 S1 - Slot Machines.py
@@ -1,28 +1,28 @@
-# Get number of quarters as input
+# get number of quarters as input
 quarters = int(input())
 
-# Get number of times each machine has been played
+# get number of times each machine has been played
 first = int(input())
 second = int(input())
 third = int(input())
 
-# Keep track of plays
+# keep track of plays
 plays = 0
 
 while True:
-  # Add plays to each machine based on number of quarters
+  # add plays to each machine based on number of quarters
   remainder = quarters % 3
   first += quarters // 3 
   second += quarters // 3
   third += quarters // 3
 
-  # Deal with left over plays after distribution
+  # deal with left over plays after distribution
   if remainder == 1: first += 1
   if remainder == 2: first += 1; second += 1
   plays += quarters
   quarters = 0
 
-  # Calculate quarters gained through wins
+  # calculate quarters gained through wins
   if first // 35 > 0:
     quarters += (first // 35)*30
     first -= 35*(first // 35)
@@ -37,7 +37,7 @@ while True:
     quarters += (third // 10)*9
     third -= 10*(third // 10)
 
-  # Stop when the number of quarters reach 0
+  # stop when the number of quarters reach 0
   if quarters == 0:
     break
 
