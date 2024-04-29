1fa09ac6c76b6a19e8c29e098ae76b49e2eb5f66
Jason He <151975056+BerryMachine@users.noreply.github.com>
2024-04-29 16:47:58 -0400

    Update CCC '09 S1 - Cool Numbers.py
    Fix comments

--- CCC '09 S1 - Cool Numbers.py
+++ CCC '09 S1 - Cool Numbers.py
@@ -4,10 +4,10 @@ b = int(input())
 num = 0
 
 for i in range(10000):
-  #For a number to a cube and square, it had to be a number with a power of 6
+  # numbers that are both a cube and square must be an integer to the power of 6.
   if i**6 > b:
     break
-  #adding to the num value if i to the 6th power is between a and b
+  # increase num if i to the 6th power is between a and b
   if a <= i**6 <= b: num += 1;
 
 
