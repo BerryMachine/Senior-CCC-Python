2a248063e44d0a60122302135c604ecc3bc688af
Jason He <151975056+BerryMachine@users.noreply.github.com>
2024-05-18 22:18:30 -0400

    Update CCC_16_S1.py
    testing

--- CCC_16_S1.py
+++ CCC_16_S1.py
@@ -9,23 +9,32 @@ dict_B = {}
 counter = 0
 
 for char in string.ascii_lowercase:
-    dict_A[char] = 0
-    dict_B[char] = 0
+  dict_A[char] = 0
+  dict_B[char] = 0
+
+# print(dict_A)
+# print(dict_B)
 
 for letter in first_word:
-    dict_A[letter] += 1
+  dict_A[letter] += 1
 
 for letter in second_word: 
+  # print(letter)
+  if letter != "*":
     dict_B[letter] += 1
 
+# print()
+# print(dict_A)
+# print(dict_B)
+
 if len(first_word) == len(second_word):
-    for char in dict_A:
-        if dict_A[char] != dict_B[char]:
-            print("N")
-            break
-            
-        else: counter += 1
+  for char in dict_A:
+    if dict_A[char] != dict_B[char]:
+      print("N")
+      break
 
-    if counter == 26: print("A")
+    else: counter += 1
+
+  if counter == 26: print("A")
+
 else: print("N")
-
