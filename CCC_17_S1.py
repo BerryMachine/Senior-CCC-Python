b5c31d8a2b9cddb122edd9c2cc08072720a09fbb
Jason He <151975056+BerryMachine@users.noreply.github.com>
2024-05-19 21:22:16 -0400

    Update CCC_17_S1.py
    Improved comments and variables

--- CCC_17_S1.py
+++ CCC_17_S1.py
@@ -1,22 +1,24 @@
-n = int(input())
+N = int(input())
 
-swi = input().split()
-sema = input().split()
+# store the number of runs each day by each time in a list
+Sw_runs = list(map(int, input().split()))
+Se_runs = list(map(int, input().split()))
 
-#the same score it always possible on day 0
-k_list = [0]
+# store all possible days K
+K_list = [0]
 
-#counting variables
-swi_total = 0
-sema_total = 0
+# counting variables
+Sw_total = 0
+Se_total = 0
 
-#determine the sum for every day and check if the score was the same or not
-for day in range(n):
-  swi_total += int(swi[day])
-  sema_total += int(sema[day])
+for day in range(N):
+  # add the runs of each team for each day
+  Sw_total += Sw_runs[day]
+  Se_total += Se_runs[day]
 
-  if swi_total == sema_total:
-    k_list.append(day+1)
+  # store day if the total runs of each team is equal
+  if Sw_total == Se_total:
+    K_list.append(day + 1)
 
-#sorted the days list and use the largest value
-print(sorted(k_list)[-1])
+# output largest value
+print(K_list[-1])
