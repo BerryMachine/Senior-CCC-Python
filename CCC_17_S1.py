14dcf99651178b2a8c45e52658b894232c4a1038
Jashan Singh <101469914+fullstakdeveloper@users.noreply.github.com>
2024-05-19 21:01:52 -0400

    Update CCC_17_S1.py

--- CCC_17_S1.py
+++ CCC_17_S1.py
@@ -3,11 +3,14 @@ n = int(input())
 swi = input().split()
 sema = input().split()
 
+#the same score it always possible on day 0
 k_list = [0]
 
+#counting variables
 swi_total = 0
 sema_total = 0
 
+#determine the sum for every day and check if the score was the same or not
 for day in range(n):
   swi_total += int(swi[day])
   sema_total += int(sema[day])
@@ -15,4 +18,5 @@ for day in range(n):
   if swi_total == sema_total:
     k_list.append(day+1)
 
+#sorted the days list and use the largest value
 print(sorted(k_list)[-1])
