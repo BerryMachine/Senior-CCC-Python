466f3e163dcfec154710b19e74130bb0ce73fede
Jashan Singh <101469914+fullstakdeveloper@users.noreply.github.com>
2024-05-21 17:01:22 -0400

    Update CCC_04_S2.py

--- S2/CCC_04_S2.py
+++ S2/CCC_04_S2.py
@@ -1,50 +1,47 @@
 n, k = input().split()
 n, k = int(n), int(k)
-
 dict = {}
 
-for i in range(n):
-  dict[i] = {"rank": 0, "score": 0, "badrank": 0}
+for value in range(n):
+  dict[value] = {"rank": 0, "score": 0, "low_rank": 0}
 
 for round in range(k):
-  scores = input().split()
+  scores  = input().split()
+
+  #adding the score into the dictionary
+  for value in range(n):
+    dict[value]["score"] += int(scores[value])
+
+  #determine the rank after every round
   rank_list = []
   
-  for change in range(n):
-    dict[change]["score"] += int(scores[change])
-    rank_list.append([change, dict[change]["score"]])
-    
-  rank_list.sort(key=lambda x: x[1])
-  
-  for value in rank_list:
-    dict[value[0]]["rank"] = n - (rank_list.index(value))
-    
-    if dict[value[0]]["rank"]> dict[value[0]]['badrank']:
-      dict[value[0]]["badrank"] = dict[value[0]]['rank']
+  for rank in range(n):
+    rank_list.append([rank, dict[rank]["score"]])
 
+  for x in rank_list:
+    j = 0
+    
+    for i in rank_list:
+      if x[1] < i[1]: j += 1
+
+    dict[x[0]]["rank"] = j + 1
+    if dict[x[0]]["rank"] > dict[x[0]]["low_rank"]:
+      dict[x[0]]["low_rank"] = dict[x[0]]["rank"] 
+
+  #determine the printing for the winner
+rank_list.sort(key=lambda x: x[1])
 win_list = []
 
-for i in range(n-1, 1, -1):
-  if rank_list[i][1] == rank_list[i-1][1]:
-    win_list.append(rank_list[i-1][1])
-  else: break
-
+for value in range(len(rank_list)-1):
+  if rank_list[-1][1] == rank_list[value][1]:
+    win_list.append(rank_list[value])
 
 win_list.append(rank_list[-1])
+
 win_list.sort(key=lambda x: x[0])
 
-x = win_list[0][0] + 1
-y = dict[win_list[0][0]]["score"]
-z = dict[win_list[0][0]]["badrank"]
-print(f"Yodeller {x} is the TopYodeller: score {y}, worst rank {z}")
-
-
-
-
-      
-    
-  
-    
-  
-
-  
+for i in range(len(win_list)):
+  x = win_list[i][0] + 1
+  y = win_list[i][1]
+  z = dict[win_list[i][0]]["low_rank"]
+  print(f"Yodeller {x} is the TopYodeller: score {y}, worst rank {z}")
