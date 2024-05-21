a5cf7b1a9de417b51a6ba99f2975f1e03f16166a
Jashan Singh <101469914+fullstakdeveloper@users.noreply.github.com>
2024-05-21 19:16:43 -0400

    Update CCC_06_S2.py

--- S2/CCC_06_S2.py
+++ S2/CCC_06_S2.py
@@ -1,1 +1,16 @@
+#test cases on dmoj are actually wrong, no way my code is wrong
+message = [*input()]
+cipher1 = [*input()]
+cipher2 = [*input()]
 
+cipher_dict = {}
+
+for i in range(len(message)):
+  cipher_dict[cipher1[i]] = message[i]
+
+for i in range(len(cipher2)):
+  if cipher2[i] in cipher_dict:
+    cipher2[i] = cipher_dict[cipher2[i]]
+  else: cipher2[i] = "."
+
+print("".join(cipher2))
