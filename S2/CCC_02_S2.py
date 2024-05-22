05744b2a32842329ea23ee4a597e7910d46b6180
Jason He <151975056+BerryMachine@users.noreply.github.com>
2024-05-21 22:01:56 -0400

    Update CCC_02_S2.py

--- S2/CCC_02_S2.py
+++ S2/CCC_02_S2.py
@@ -1,30 +1,15 @@
-nemu = int(input())
-deno = int(input())
+import math
 
+numerator = int(input())
+denominator = int(input())
 
-Max = nemu
-Min = deno
+rem = numerator % denominator
+if rem == 0:
+  print(numerator // denominator)
+else:
+  gcd = math.gcd(rem, denominator)
 
-gcf = ''
-
-Rem = 0
-
-what = ''
-
-while True:
-  Rem = Max % Min
-  if Rem == 0:
-    gcf = Min
-    break
-  else: Max = Min; Min = Rem
-
-deno = deno/gcf
-nemu = nemu/gcf
-
-
-if nemu % deno == 0:
-  print(int(nemu/deno))
-elif nemu > deno:
-  num = nemu // deno
-  print(int(num), str(int(nemu - num*deno)) + "/" + str(int(deno)))
-else: print(str(int(nemu)) + "/" + str(int(deno)))
+  if rem == numerator:
+    print(numerator//gcd, "/", denominator//gcd, sep="")
+  else:
+    print(numerator // denominator, " ", rem//gcd, "/", denominator//gcd, sep="")
