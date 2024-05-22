f814b0229d18a08c5b4e8419931f5b0e25b6c6d3
Jason He <151975056+BerryMachine@users.noreply.github.com>
2024-05-21 22:04:45 -0400

    Update CCC_02_S2.py
    Add comments

--- S2/CCC_02_S2.py
+++ S2/CCC_02_S2.py
@@ -1,15 +1,24 @@
 import math
 
+# get fraction as input
 numerator = int(input())
 denominator = int(input())
 
+# calculate remainder after division
 rem = numerator % denominator
+
+# if fraction is integer, output integer
 if rem == 0:
   print(numerator // denominator)
+  
 else:
+  # calculate gcd to reduce fraction
   gcd = math.gcd(rem, denominator)
 
+  # if fraction < 1, output reduced fraction
   if rem == numerator:
     print(numerator//gcd, "/", denominator//gcd, sep="")
+    
+  # if fraction > 1, output reduced mixed fraction
   else:
     print(numerator // denominator, " ", rem//gcd, "/", denominator//gcd, sep="")
