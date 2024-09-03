79dd505ff497c3f9ad2e18124d4c96e976ddb467
Jason He <151975056+BerryMachine@users.noreply.github.com>
2024-09-03 18:04:38 -0400

    Update CCC_18_S1.py
    fix terribly written comments >:(

--- S1/CCC_18_S1.py
+++ S1/CCC_18_S1.py
@@ -1,36 +1,36 @@
 from math import *
 n = int(input())
 
-#villages coordinate list
+# villages coordinate list
 vils = []
 
-#populations of the villages list
+# populations of the villages list
 pops = []
 
-#adding values into the villages list from input
+# add values into the villages list from input
 for vek in range(n):
   vil = int(input())
   vils.append(vil)
 
-#sorteding village coordinates to resemble numberline
+# sort village coordinates to resemble numberline
 vils = sorted(vils)
 
-#A is the city before B
-#B is the city being compared
-#C is the city after B
-#note: the first and last city in the village list don't matter
+# A is the city before B
+# B is the city in question
+# C is the city after B
+# **note**: we can disregard the first and last city in the village list 
 for i in range(1, len(vils)-1):
   A = (vils)[i-1]
   B = (vils)[i]
   C = (vils)[i+1]
 
-  #used the difference of the values to get population
+  # use the difference of the values to get population
   pop = abs(B - A)/2 + abs(C-B)/2
 
-  #added to the population list
+  # add to the population list
   pops.append(pop)
 
-#printed the sorted version fo pops, the smallest value
+# output the sorted version fo pops, the smallest value
 print(float(sorted(pops)[0]))
  
 
