3ce0bf34f3911996aca8c75f8c62da02413fb01d
Jason He <151975056+BerryMachine@users.noreply.github.com>
2024-09-04 23:39:10 -0400

    Update CCC_19_S1.py
    optimize solution

--- S1/CCC_19_S1.py
+++ S1/CCC_19_S1.py
@@ -1,33 +1,24 @@
-things = [*input()]
+flip_seq = input()
 
-#initial list
-list = [1, 2, 3, 4]
+# count number of each H and V flips
+H_count = flip_seq.count("H")
+V_count = flip_seq.count("V")
 
-#dictionary for horizontal and vertical flip commands
-dict = {'H': 0, 'V': 0}
+# every 2 flips in the same direction cancel out
+# therefore, we only need to consider the (count mod 2) flip(s) in each direction
+# output the respective grid layout
+if H_count % 2 == 0:
+    if V_count % 2 == 0:
+        print("1 2")
+        print("3 4")
+    else:
+        print("2 1")
+        print("4 3")
 
-#adding to the dict based on things input of H and V
-for letter in things: 
-  if letter == 'H': dict['H'] += 1
-  else: dict['V'] += 1
-
-#determining if the number of H and V is even or odd, because 2 H/V's returns the set to its original state
-mod_h = dict['H'] % 2
-mod_v = dict['V'] % 2
-
-#brute forced test cases because there are only 4
-if mod_h == 1 and mod_v == 1:
-  print(4, 3)
-  print(2, 1)
-
-elif mod_h == 1:
-  print(3, 4)
-  print(1, 2)
-
-elif mod_v == 1:
-  print(2, 1)
-  print(4, 3)
-
-else: 
-  print(1, 2)
-  print(3, 4)
+else:
+    if V_count % 2 == 0:
+        print("3 4")
+        print("1 2")
+    else:
+        print("4 3")
+        print("2 1")
