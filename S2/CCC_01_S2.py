77c2e534a3c5920dfface7b1b20bb0d34e51d8df
Jason He <151975056+BerryMachine@users.noreply.github.com>
2024-09-06 18:19:25 -0400

    Update CCC_01_S2.py
    improved solution + comments

--- S2/CCC_01_S2.py
+++ S2/CCC_01_S2.py
@@ -1,69 +1,58 @@
-import math
-
-start_int = int(input())
-end_int = int(input())
-width = 0
-check_width = 0
-height = 1
-check_height = 1
-
-while True:
-  width += 1
-  check_width += width * 2
-  if end_int - start_int < check_width:
-    break
-
-while True:
-  if end_int - start_int < check_height:
-    break
-  check_height += height * 2 + 1
-  height += 1
-
-x_mid = math.ceil(width/2)
-y_mid = math.ceil(height/2)
-
-nums = {}
-nums[start_int] = [x_mid, y_mid]
-
-w = 2
-ww = 1
-h = 1
-hh = 1
-direction_w = 1
-direction_h = 1
-horizontal = -1
-n = start_int
-x = x_mid
-y = y_mid
-
-for i in range(end_int - start_int):
-  n += 1
-  if horizontal == 1:
-    x += direction_w
+# adjust direction matrix to make a left turn in spiral
+def leftTurn(directions, count_to_turn):
+  if directions[0] == 0:
+    directions[0] = -directions[1]
+    directions[1] = 0
+    return directions, count_to_turn
+  
   else:
-    y += direction_h
+    directions[1] = directions[0]
+    directions[0] = 0
+    # increase length of countdown to next left turn by 1 after moving horizontally
+    return directions, count_to_turn + 1
 
-  nums[n] = [x, y]
+# get start and end integer as input
+start = int(input())
+end = int(input())
+# starting coordinates
+x = 0
+y = 0
+# keep track of numbers in each row to facilitate output formatting
+rows = {y: [start]}
+# adjusting matrix to generate spiral
+directions = [0, -1]
+# count to left turn in spiral
+count = 0
+count_to_turn = 1
 
-  if n - start_int == w:
-    ww += 1
-    w = w + ww*2
-    horizontal *= -1
-    direction_w *= -1
+# generate spiral using coordinates
+for i in range(start+1, end+1):
+  # add adjusting matrix
+  x += directions[0]
+  y += directions[1]
+  
+  # save integer in its corresponding row
+  if y in rows.keys():
+    # **Note**: an integer is always placed on the far right or left in its row
+    # determine where to store the integer in the row
+    if directions[0] == 1 or directions[1] == 1:
+      rows[y].append(i)
+    else:
+      rows[y].insert(0, i)
 
-  elif n - start_int == h:
-    h = h + hh*2 + 1
-    hh += 1
-    horizontal *= -1
-    direction_h *= -1
+  else:
+    rows[y] = [i]
 
-for i in range(1, height+1):
-  list = []
-  for j in range(1, width+1):
-    coordinates = [j, i]
-    for num in nums:
-      if coordinates == nums[num]:
-        list.append(num)
+  # increment count
+  count += 1
 
-
-  print(*list)
+  # perform left turn
+  if count == count_to_turn:
+    directions, count_to_turn = leftTurn(directions, count_to_turn)
+    count = 0
+  
+# output each row from top to botom
+min_x = -(len(rows)//2) - 1
+max_x = len(rows) + min_x
+for j in range(max_x, min_x, -1):
+  print(*rows[j])
